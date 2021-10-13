7e261671119e9c6c9eea4614939d3283c0cd9352
Aashish Dhakal <ashish.dhakal07@gmail.com>
2021-10-13 14:53:01 +0545

    security and before deploying

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -16,10 +17,14 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
-
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const { contentSecurityPolicy } = require('helmet')
+const MongoDBStore = require('connect-mongo')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 main().then(data => console.log('Database connected!')).catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    await mongoose.connect(dbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
@@ -32,18 +37,90 @@ app.set('views', path.join(__dirname, '/views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
-app.use(session({
-    secret: 'secret',
+const secret = process.env.SECRET || 'secret'
+
+const store = MongoDBStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 3600 //time is in seconds
+})
+
+store.on('error', function (e) {
+    console.log('Session Store Error', e)
+})
+
+const sessionConfig = {
+    store,
+    name: 'sessionName',//basically change this to any name other than the default!
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000, //these are in milliseconds!
     },
-}))
+}
+
+app.use(session(sessionConfig))
 app.use(flash())
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudfare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/"
+// ];
+
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com",
+//     "https://events.mapbox.com/"
+// ];
+
+// const fontSrcUrls = [];
+
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives:{
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'","'self'",...scriptSrcUrls],
+//             styleSrc: ["'self'","'unsafe-inline'",...styleSrcUrls],
+//             workerSrc: ["'self'","blob:"],
+//             objectSrc:[],
+//             imgSrc:[
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/aashishdhakal/",
+//                 "https://images.unsplash.com/"
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls]
+//         }
+//     })
+// )
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -52,6 +129,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
+    console.log(req.query)
     res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
--- yelpCamp/schemas.js
+++ yelpCamp/schemas.js
@@ -1,4 +1,30 @@
-const Joi = require('joi')
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (joi) => ({
+    type:'String',
+    base: joi.string(),
+    messages:{
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules:{
+        escapeHTML:{
+            method(){
+                
+            },
+            validate(value, helpers){
+                const clean = sanitizeHtml(value,{
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if(clean !== value) return helpers.error('string.escapeHTML',{value})
+                return clean;
+            }
+        }
+    }
+})
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
